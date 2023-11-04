ed43c13729c524b3b883c2de5fcdea7375594bab
Thomas Kittelmann <thomas.kittelmann@ess.eu>
2023-11-04 15:17:38 +0100

    Move BasicExamples and FortranExamples to dgcode_val

---
+++ Examples/BasicExamples/python/anotherpuremodule.py
@@ -0,0 +1,5 @@
+from __future__ import print_function
+
+
+def somefunc():
+    print("somefunc in BasicExamples.anotherpuremodule")
---
+++ Examples/BasicExamples/python/somepuremodule.py
@@ -0,0 +1,4 @@
+from __future__ import print_function
+
+def somefunc():
+    print("somefunc in BasicExamples.somepuremodule")
